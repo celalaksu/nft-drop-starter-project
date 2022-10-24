f617279872b8a70d60866d9c850adc8ae45cae9b
Celal AKSU <celalaksu@gmail.com>
2022-10-24 21:20:41 +0300

    final

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -5,15 +5,20 @@ import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
 import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
 
+import { SolflareWalletAdapter, TorusWalletAdapter } from "@solana/wallet-adapter-wallets";
+
 import "../styles/App.css";
 import "../styles/globals.css";
 import "../styles/CandyMachine.css";
+import "../styles/CountdownTimer.css";
 import "@solana/wallet-adapter-react-ui/styles.css";
 
 const App = ({ Component, pageProps }) => {
     const network = WalletAdapterNetwork.Devnet;
     const endpoint = useMemo(() => clusterApiUrl(network), [network]);
-    const wallets = useMemo(() => [new PhantomWalletAdapter()], [network]);
+    // const wallets = useMemo(() => [new PhantomWalletAdapter()], [network]); // just for phantom wallet
+    const wallets = useMemo(() => [new PhantomWalletAdapter(), new SolflareWalletAdapter(), new TorusWalletAdapter()], [network]); // for all wallets
+
 
     return (
         <ConnectionProvider endpoint={endpoint}>
--- app/pages/index.js
+++ app/pages/index.js
@@ -1,19 +1,47 @@
 import React from "react";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+
+import { useWallet, WalletProvider } from "@solana/wallet-adapter-react";
+import { WalletConnectWalletName } from "@solana/wallet-adapter-wallets";
+
+import CandyMachine from "../components/CandyMachine";
 
 // Constants
-const TWITTER_HANDLE = "_buildspace";
+const TWITTER_HANDLE = "ksacll";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const Home = () => {
+    const wallet = useWallet();
+    
+    if (wallet.toString() != false)  {
+        console.log("Phantom wallet found");
+        if (wallet.publicKey){
+        console.log(wallet.publicKey.toString());
+    }
+    }
+
+    // Actions
+    const renderNotConnectedContainer = () => (
+        <div>
+            <img src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif" alt="emoji" />
+
+            <div className="button-container">
+                <WalletMultiButton className="cta-button connect-wallet-button" />
+            </div>
+        </div>
+    );
+
     return (
         <div className="App">
             <div className="container">
                 <div className="header-container">
                     <p className="header">🍭 Candy Drop</p>
                     <p className="sub-text">NFT drop machine with fair mint</p>
+                    {/* Render your connect to wallet button right here */}
+                    {wallet.publicKey ? <CandyMachine walletAddress={wallet} />  : renderNotConnectedContainer()}
                 </div>
                 <div className="footer-container">
-                    <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+                    <img alt="Twitter Logo" className="twitter-logo" src="twitter-logo.svg" />
                     <a className="footer-text" href={TWITTER_LINK} target="_blank" rel="noreferrer">{`built on @${TWITTER_HANDLE}`}</a>
                 </div>
             </div>
